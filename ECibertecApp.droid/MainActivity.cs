bd803ecd48578ec3a9b511b2f266c7ba80411038
moises.sullca <moises.sullca@GYSCPU049>
2017-11-22 17:10:10 -0500

    v1-menu

--- ECibertecApp.droid/MainActivity.cs
+++ ECibertecApp.droid/MainActivity.cs
@@ -1,18 +1,45 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V4.Widget;
+using SupportFragment = Android.Support.V4.App.Fragment;
+using SupportFragmentManager = Android.Support.V4.App.FragmentManager;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+using SupportActionBar = Android.Support.V7.App.ActionBar;
+using Android.Support.V7.App;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.App;
+using System.Collections.Generic;
+using Java.Lang;
 
 namespace ECibertecApp.droid
 {
     [Activity(Label = "ECibertecApp.droid", MainLauncher = true , Theme = "@style/Theme.DesignDemo")]
-    public class MainActivity : Activity
+    public class MainActivity : AppCompatActivity //cambiar a AppCompatActivity para usar el SetSupportActionBar,SupportActionBar
     {
+      //  private DrawerLayout mDrawerLayout;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+            //MENU 
+            SupportToolbar toolBar = FindViewById<SupportToolbar>(Resource.Id.toolBar);
+            SetSupportActionBar(toolBar);
+
+            SupportActionBar ab = SupportActionBar;
+            ab.SetHomeAsUpIndicator(Resource.Drawable.login);
+            ab.SetDisplayHomeAsUpEnabled(true);
+
+
+            //MENU
         }
     }
 }
